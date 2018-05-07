2e1467b8f66390c348e8ca355f8a162771126b00
Koteswarao <kotesh@kvana.com>
2018-05-08 00:59:30 +0530

    Fixed tab layout scrolling and it placed in viewpager.

--- app/src/main/java/com/koti/apple/tabviewscrollablestable/PagerAdapter.java
+++ app/src/main/java/com/koti/apple/tabviewscrollablestable/PagerAdapter.java
@@ -1,46 +1,69 @@
 package com.koti.apple.tabviewscrollablestable;
 
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 /**
- * Adding class fragments.
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages. Adding class fragments.
  */
-public class PagerAdapter extends FragmentStatePagerAdapter {
+public class PagerAdapter extends FragmentPagerAdapter {
     int mNumOfTabs;
 
-    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+    // tab titles
+    private String[] tabTitles = new String[]{"Tab1", "Tab2", "Tab3", "Tab4"};
+    private int[] tabImages = new int[]{R.drawable.android_m, R.drawable.android_n, R.drawable.android_o, R.drawable.android_p};
+
+    public PagerAdapter(FragmentManager fm) {
         super(fm);
-        this.mNumOfTabs = NumOfTabs;
     }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+
+    // TODO:- Try to set the images for the tab along with the text ok na.
+    public int[] getTabImages() {
+        return tabImages;
+    }
+
 
     @Override
     public Fragment getItem(int position) {
-
+        FirstFragment firstFragment = null;
         switch (position) {
             case 0:
-                FirstFragment firstFragment = new FirstFragment();
+                Log.e("info >> ", "getItem: >> "+position);
+                firstFragment = new FirstFragment();
                 return firstFragment;
             case 1:
+                Log.e("info >> ", "getItem: >> "+position);
                 SecondFragment secondFragment = new SecondFragment();
                 return secondFragment;
             case 2:
+                Log.e("info >> ", "getItem: >> "+position);
                 ThirdFragment thirdFragment = new ThirdFragment();
                 return thirdFragment;
 
             case 3:
+                Log.e("info >> ", "getItem: >> "+position);
                 FourthFragment fourthFragment = new FourthFragment();
                 return fourthFragment;
             default:
+                Log.e("info >> ", "getItem: >> ");
                 return null;
         }
+//        return firstFragment;
     }
 
     @Override
     public int getCount() {
-        return mNumOfTabs;
+        return tabTitles.length;
     }
 }
